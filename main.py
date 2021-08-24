2e95f29b45ab74eec8282018397a628b16cc3e10
BuildTools <unconfigured@null.spigotmc.org>
2021-08-24 19:16:18 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+from discord import Webhook, RequestsWebhookAdapter
+
+url = "https://discord.com/api/webhooks/879775144342614097/" \
+      "-lBTYBVecM-DPeQMgK7qyNaXwvwCKsLmwJ1CM26EMIiR8XwhatLctSjilPkZX-HC6bs5"
+
+print(requests.post(url, data="wowowow"))
